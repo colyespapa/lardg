0f7ce98c52edb69d8f86f0e3310864b6bc1be6e4
Gerente Col Yes Papa <jlbislas@internet.ru>
2021-08-27 22:41:34 -0500

    colish

---
+++ code.js
@@ -0,0 +1,9 @@
+onEvent("button2", "click", function( ) {
+  open("http://translate.coloficial.ml");
+});
+onEvent("button1", "click", function( ) {
+  open("https://bank.coloficial.ml");
+});
+onEvent("button3", "click", function( ) {
+  open("http://tienda.coloficial.ml");
+});
