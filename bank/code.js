2c36aa6fc4dbdbb81a2a3daa3263668e610d68cd
Gerente Col Yes Papa <jlbislas@internet.ru>
2021-09-08 20:26:04 -0500

    update

--- bank/code.js
+++ bank/code.js
@@ -1,9 +1,9 @@
 onEvent("button1", "click", function( ) {
-  open("https://www.lardcorporation.uk.eu.org/bank/statement/index.html");
+  open("https://www.lardcorporation.uk.eu.org/statement/index.html");
 });
 onEvent("button2", "click", function( ) {
-  open("https://www.lardcorporation.uk.eu.org/bank/cardgenerator/index.html");
+  open("https://www.lardcorporation.uk.eu.org/cardgenerator/index.html");
 });
 onEvent("button3", "click", function( ) {
-  open("https://www.lardcorporation.uk.eu.org/bank/checkbook/index.html");
+  open("https://www.lardcorporation.uk.eu.org/checkbook/index.html");
 });
