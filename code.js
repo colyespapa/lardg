33f1857df417f11c67269a7d72882e82583f2938
Gerente Col Yes Papa <jlbislas@internet.ru>
2021-11-07 10:14:45 -0600

    Update website

--- code.js
+++
@@ -1,9 +0,0 @@
-onEvent("button1", "click", function( ) {
-  open("http://lardcorporation.uk.eu.org/webapps/index.html");
-});
-onEvent("button2", "click", function( ) {
-  setScreen("screen2");
-});
-onEvent("button3", "click", function( ) {
-  setScreen("screen1");
-});
