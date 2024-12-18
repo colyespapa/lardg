787b9c7421d04b6d80dd07c746be31cd82d02d52
Lard Corporation <jl.lard.2018@gmail.com>
2024-12-17 22:40:34 -0600

    Delete

--- old/old/website/dictionary_ver/code.js
+++
@@ -1,9 +0,0 @@
-onEvent("button1", "click", function( ) {
-  open("https://www.lardcorporation.uk.eu.org/old/dictionary/index.html");
-});
-onEvent("button2", "click", function( ) {
-  open("https://www.lardcorporation.uk.eu.org/dictionary/index.html");
-});
-onEvent("button3", "click", function( ) {
-  open("https://www.lardcorporation.uk.eu.org");
-});
